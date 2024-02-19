f8a914ad409b93a6a1b674a74b32c5bb98046033
architectnt <eno@i2iproductions.com>
2024-02-18 22:58:09 -0800

    add a bit of a 'lib' next to defs that isn't windows

--- include/tests.h
+++ include/tests.h
@@ -1,7 +1,7 @@
 #ifdef _WIN32
 #define LIB_AAFC_RPATH "aafc.dll"
 #elif __APPLE__
-#define LIB_AAFC_RPATH "./aafc.dylib"
+#define LIB_AAFC_RPATH "./libaafc.dylib"
 #else
-#define LIB_AAFC_RPATH "./aafc.so"
+#define LIB_AAFC_RPATH "./libaafc.so"
 #endif
