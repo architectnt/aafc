56da63ddb6cadbc1b0115f99cdd49fed7b4b5f96
architectnt <eno@i2iproductions.com>
2025-05-14 22:52:03 -0700

    why

---
+++ src/aafc/common.h
@@ -0,0 +1,10 @@
+/*
+    Copyright (C) 2024-2025 Architect Enterprises
+    This file is apart of AAFC and is licenced under the MIT Licence.
+*/
+
+float smoothInterpolate(float y0, float y1, float y2, float y3, double t);
+unsigned char minifloat(float val);
+unsigned short halfpercision(float val);
+float dminif(unsigned char val);
+float dhalf(unsigned short val);
